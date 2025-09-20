e81ce246261938ebbd0838bc916ce6fcd44984a3
Albert <albert0130@proton.me>
2025-09-20 11:09:40 +0900

    Vercel deploy 2

--- main.py
+++ main.py
@@ -23,3 +23,6 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
+# For Vercel deployment
+handler = app
